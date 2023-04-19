16a5065bfefe8e50a2cca63672143822a27cce67
Juan José García <jitos@jitos.dev>
2023-04-19 17:10:19 +0200

    first commit block1-process

---
+++ Block1-process-file-and-streams/src/main/java/dev/jitos/block1processfileandstreams/InvalidLineFormatException.java
@@ -0,0 +1,8 @@
+package dev.jitos.block1processfileandstreams;
+
+public class InvalidLineFormatException extends Exception{
+
+    public InvalidLineFormatException(String message) {
+        super(message);
+    }
+}
---
+++ Block1-process-file-and-streams/src/main/java/dev/jitos/block1processfileandstreams/Person.java
@@ -0,0 +1,39 @@
+package dev.jitos.block1processfileandstreams;
+
+public class Person {
+
+    private String name;
+    private String town;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", town='" + town + '\'' +
+                ", age='" + age + '\'';
+    }
+}
