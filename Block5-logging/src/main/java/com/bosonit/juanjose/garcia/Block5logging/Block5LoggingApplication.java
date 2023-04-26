78e0e9405e95749555e424d7a61379b31153c6ef
Juan José García <jitos@jitos.dev>
2023-04-26 16:26:06 +0200

    finish Block5-loggin

--- Block5-logging/src/main/java/com/bosonit/juanjose/garcia/Block5logging/Block5LoggingApplication.java
+++ Block5-logging/src/main/java/com/bosonit/juanjose/garcia/Block5logging/Block5LoggingApplication.java
@@ -3,11 +3,21 @@ package com.bosonit.juanjose.garcia.Block5logging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class Block5LoggingApplication {
 
+	static Logger logger = LoggerFactory.getLogger(Block5LoggingApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(Block5LoggingApplication.class, args);
+
+		logger.trace("Trace desde main..");
+		logger.warn("Warn desde main..");
+		logger.error("Error desde main..");
 	}
 
 }
