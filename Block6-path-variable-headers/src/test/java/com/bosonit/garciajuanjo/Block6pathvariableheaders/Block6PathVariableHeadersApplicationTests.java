d42dc6d00fe89096d3496e52a4a0bbd15c409a10
Juan José García <jitos@jitos.dev>
2023-05-02 16:06:57 +0200

    first commit block6-path-variable-headers

---
+++ Block6-path-variable-headers/src/test/java/com/bosonit/garciajuanjo/Block6pathvariableheaders/Block6PathVariableHeadersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.garciajuanjo.Block6pathvariableheaders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block6PathVariableHeadersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
