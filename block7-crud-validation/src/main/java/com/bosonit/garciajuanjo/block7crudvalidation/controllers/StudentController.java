b526839e8b4eaf4e266a46866a5a981c09fd16b5
Juan José García <jitos@jitos.dev>
2023-05-05 14:10:20 +0200

    add Teacher and Student controllers

---
+++ block7-crud-validation/src/main/java/com/bosonit/garciajuanjo/block7crudvalidation/controllers/StudentController.java
@@ -0,0 +1,9 @@
+package com.bosonit.garciajuanjo.block7crudvalidation.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("student")
+public class StudentController {
+}
