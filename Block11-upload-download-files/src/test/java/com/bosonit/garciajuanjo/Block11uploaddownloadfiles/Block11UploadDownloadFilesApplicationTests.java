cda14dcd231d1d949b15f51de26960b2f28b09bf
Juan José García <jitos@jitos.dev>
2023-05-22 12:31:13 +0200

    create proyect Block11-upload-download-files

---
+++ Block11-upload-download-files/src/test/java/com/bosonit/garciajuanjo/Block11uploaddownloadfiles/Block11UploadDownloadFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.garciajuanjo.Block11uploaddownloadfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block11UploadDownloadFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
