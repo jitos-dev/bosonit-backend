8dc4c1f833f24a13704d28a667538c82a3af9304
Juan José García <jitos@jitos.dev>
2023-05-30 12:36:22 +0200

    add Mockito dependency in the proyect Block7-crud-validation

--- block7-crud-validation/src/main/java/com/bosonit/garciajuanjo/block7crudvalidation/services/impl/PersonServiceImpl.java
+++ block7-crud-validation/src/main/java/com/bosonit/garciajuanjo/block7crudvalidation/services/impl/PersonServiceImpl.java
@@ -17,6 +17,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
--- block7-crud-validation/src/test/java/com/bosonit/garciajuanjo/block7crudvalidation/services/PersonServiceImplTest.java
+++ block7-crud-validation/src/test/java/com/bosonit/garciajuanjo/block7crudvalidation/services/PersonServiceImplTest.java
@@ -1,12 +1,61 @@
 package com.bosonit.garciajuanjo.block7crudvalidation.services;
 
 import com.bosonit.garciajuanjo.block7crudvalidation.entities.dto.PersonInputDto;
+import com.bosonit.garciajuanjo.block7crudvalidation.exceptions.UnprocessableEntityException;
+import com.bosonit.garciajuanjo.block7crudvalidation.services.impl.PersonServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+
+import java.util.Date;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 
 public class PersonServiceImplTest {
+    public static PersonInputDto personInputDto;
+    public static PersonInputDto personInputDtoNull;
+
+    @BeforeAll
+    public static void initPersonInputDto() {
+        personInputDto = new PersonInputDto(
+                "aaa",
+                "usuario1",
+                "123456",
+                "juanjo",
+                "garcia",
+                "juanjose.garcia@bosonit.com",
+                "jitos86@gmail.com",
+                "Sabiote",
+                true,
+                new Date(),
+                "http://localhost:8080/imagen1",
+                new Date());
+
+        personInputDtoNull = new PersonInputDto(
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null);
+    }
 
     @Test
-    public void isAllFieldsCorrectTest(PersonInputDto inputDto) {
+    public void isAllFieldsCorrectTest() {
+        //assertThat(personInputDto.getUser(), equalTo("usuario1"));
+        //assertThat(personInputDto.getUser(), notNullValue());
 
+        Assertions.assertThrows(UnprocessableEntityException.class, () -> {
+
+        });
     }
 }
