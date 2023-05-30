5265ef2be254c0a4c9a419813ca6eb13888f7d9c
Juan José García <jitos@jitos.dev>
2023-05-30 11:12:50 +0200

    add Hamcrest dependency in the proyect Block7-crud-validation

---
+++ block7-crud-validation/src/test/java/com/bosonit/garciajuanjo/block7crudvalidation/services/PersonServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.bosonit.garciajuanjo.block7crudvalidation.services;
+
+import com.bosonit.garciajuanjo.block7crudvalidation.entities.dto.PersonInputDto;
+import org.junit.jupiter.api.Test;
+
+public class PersonServiceImplTest {
+
+    @Test
+    public void isAllFieldsCorrectTest(PersonInputDto inputDto) {
+
+    }
+}
